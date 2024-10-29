eb08e5d9687689a776d2451a826d425f2b6d7844
= <luis.moo.huert@gmail.com>
2024-10-29 00:03:50 -0600

    Done

--- exercise.js
+++ exercise.js
@@ -11,7 +11,7 @@
 /** DO   CHANGE THE FUNCTION NAME **/
 const cToF = (c) => {
     /* Only make changes below this comment */
-    
+    return(c)= c * 9/5 + 32
 
     /* Only make changes below this comment */
 }
